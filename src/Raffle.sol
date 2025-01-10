13c8a91699811462979ec57af1105429ab53a69b
Simon Jonsson <simonjonsson87@gmail.com>
2025-01-10 20:15:52 +0000

    First commit. The basic Raffle contract is there, but there is no automation

---
+++ src/Raffle.sol
@@ -0,0 +1,140 @@
+// SPDX-License_Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {VRFCoordinatorV2Interface} from "chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+
+import {VRFConsumerBaseV2Plus} from "chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+error Raffle__NotEnoughEthSent();
+error Raffle__TransferFailed();
+error Raffle__NotOpen();
+
+/**
+ * @title A simple raffle contract for the Patric Collins Foundry Fundamentals course
+ * @author Simon Jonsson
+ * @notice Users will be able to get a ticket, and possibly win.
+ */
+contract Raffle is VRFConsumerBaseV2Plus {
+
+    /* Type Declarations */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
+    /* State variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+    address payable private s_recentWinner;
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
+    RaffleState private s_raffleState;
+
+    // Chainlink VRF related variables
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_keyHash;
+    uint32 private immutable i_callbackGasLimit;
+
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    event EnterredRaffle(address indexed player);
+    event WinnerPicked(address indexed winner);
+
+    constructor(
+        bytes32 keyHash,
+        bytes32 gasLane,
+        uint32 callbackGasLimit,
+        uint64 subscriptionId,
+        uint256 interval,
+        uint256 entranceFee,
+        address vrfCoordinatorV2
+    ) VRFConsumerBaseV2Plus(vrfCoordinatorV2) {
+        i_gasLane = gasLane;
+        i_interval = interval;
+        i_subscriptionId = subscriptionId;
+        i_entranceFee = entranceFee;
+        s_lastTimeStamp = block.timestamp;
+        i_callbackGasLimit = callbackGasLimit;
+        i_keyHash = keyHash;
+        s_raffleState = RaffleState.OPEN;
+    }
+
+    function enterRaffle() external payable {
+        //require(msg.value >= i_entranceFee, "You need to send more ETH to paricipate");
+        if (msg.value < i_entranceFee) revert Raffle__NotEnoughEthSent();
+        if (s_raffleState == RaffleState.CALCULATING) revert Raffle__NotOpen();
+        s_players.push(payable(msg.sender));
+        emit EnterredRaffle(msg.sender);
+    }
+
+    function pickWinner() public {
+        if (block.timestamp - s_lastTimeStamp < i_interval) revert();
+        if (s_raffleState == RaffleState.CALCULATING) revert Raffle__NotOpen();
+
+        s_raffleState = RaffleState.CALCULATING;
+
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false}))
+            })
+        );
+        
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal virtual override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+
+        s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+
+        (bool success,) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert();
+        }
+        emit WinnerPicked(recentWinner);
+    }
+
+    //==========================================================
+    // Getter functions
+    //==========================================================
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
+
+// Layout of the contract file:
+// version
+// imports
+// errors
+// interfaces, libraries, contract
+
+// Inside Contract:
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+
+// view & pure functions
